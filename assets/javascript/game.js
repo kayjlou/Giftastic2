47ce06045659605354f5b41c8e7003ff58eb6641
Katie Lewis <klewi314@gmail.com>
2019-03-20 21:16:05 -0700

    Added fetch api

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,31 @@
 //array of buttons
 let topics = ["dogs", "cats", "penguins"];
 
-//Function to display gify url into the html
-
-let animal = this.attr("data-name");
-
-let queryURL = `https://api.giphy.com/v1/gifs/search?q="{animal}&api_key=dc6zaTOxFJmzC&limit=10`;
-
 //Fetch
 fetch(
-  'https://api.giphy.com/v1/gifs/search?q="{animal}&api_key=dc6zaTOxFJmzC&limit=10'
+  // let animal = this.attr("data-name")
+  "https://api.giphy.com/v1/gifs/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10"
 )
-.then(r => {
-  r.json())
-})
+  .then(r => r.json())
+  .then(r => {
+    console.log(
+      r.data.forEach(gifObject => {
+        //Create new image tag
+        imgElem = document.createElement("img");
+        //Define rating
+        rating = gifObject.rating;
+        //Animate or still variables
+        let urlStill = gifObject.images.fixed_height_still.url;
+        let urlAnimate = gifObject.images.fixed_height.url;
+        let ratingPtag = (document.createElement("p").innerHTML = "Rating is ");
+
+        //Display non moving
+
+        imgElem.setAttribute("src", urlAnimate);
+        imgElem.setAttribute("data-animate", urlAnimate);
+        imgElem.setAttribute("data-still", urlStill);
+        imgElem.setAttribute("data-state", "animate");
+        document.querySelector("#gif-view").append(imgElem, ratingPtag);
+      })
+    );
+  });
