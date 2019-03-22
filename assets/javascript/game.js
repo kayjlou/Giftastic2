63f0ad06150b7f3942aad7257fd579cda3470f60
Katie Lewis <klewi314@gmail.com>
2019-03-21 20:29:48 -0700

    added button function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,17 @@
 //array of buttons
-let topics = ["dogs", "cats", "penguins"];
+const makeButtons = () => {
+  let topics = ["dogs", "cats", "penguins"];
+  //Empty buttons
+  // document.querySelector("#buttons-view").empty();
+  //Loop through array of items
+  for (i = 0; i < topics.length; i++) {
+    let btn = document.createElement("button");
+    btn.className = "buttons";
+    btn.textContent = topics[i];
+    document.querySelector("#buttons").append(btn);
+  }
+};
+makeButtons();
 
 const searchGif = () => {
   //Prevent form from refreshing
@@ -9,6 +21,7 @@ const searchGif = () => {
     document.querySelector("#animal").value.length >= 3
       ? document.querySelector("#animal").value
       : "Kittens";
+
   //Fetch
   fetch(
     `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=dc6zaTOxFJmzC&limit=10`
@@ -16,6 +29,7 @@ const searchGif = () => {
     .then(r => r.json())
     .then(r => {
       document.querySelector("#text").innerHTML = `<h4> ${animal} </h4>`;
+
       //iterates through elements
       r.data.forEach(gifObject => {
         //Create new image tag
@@ -25,17 +39,35 @@ const searchGif = () => {
         //Animate or still variables
         let urlStill = gifObject.images.fixed_height_still.url;
         let urlAnimate = gifObject.images.fixed_height.url;
-        // let ratingTag = (document.createElement(
-        //   "span"
-        // ).innerHTML = `Rating is ${rating}`);
 
-        //Display non moving
-
+        //Display moving
         imgElem.setAttribute("src", urlAnimate);
+        imgElem.setAttribute("className", "image");
         imgElem.setAttribute("data-animate", urlAnimate);
         imgElem.setAttribute("data-still", urlStill);
         imgElem.setAttribute("data-state", "animate");
+        imgElem.addEventListener(
+          "click",
+          (pauseGif = () => {
+            // let state = imgElem.setAttribute("date-state", "animate");
+            // console.log("animate")
+            let state = imgElem.dataset.state;
+            console.log(state);
+            if (state === "animate") {
+              //set src to still image
+              console.log("this is animated");
+              imgElem.setAttribute("src", urlStill);
+              imgElem.setAttribute("data-state", "still");
+            } else if (state === "still") {
+              //Set image back to animated
+              console.log("this is still");
+              imgElem.setAttribute("src", urlAnimate);
+              imgElem.setAttribute("data-state", "animate");
+            }
+          })
+        );
         document.querySelector("#gif-view").append(imgElem);
       });
     });
+  //function for when image is clicked on to pause
 };
