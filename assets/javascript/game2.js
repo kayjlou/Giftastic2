75204dc92395e44133c0702a51f03a1fd391a952
Katie Lewis <klewi314@gmail.com>
2019-03-25 18:36:59 -0700

    Updated js

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -3,22 +3,29 @@ let topics = ["dogs"];
 let isAnimated = true;
 
 //Gather text input
-document.querySelector("#submit").addEventListener("click", e => {
+document.querySelector("#animalBtn").addEventListener("click", e => {
   //Prevent reload
   e.preventDefault();
+  document.querySelector("#buttons").empty();
   //check for duplicate or not valid submission
-
+  let animal = document.querySelector("#add-animal").value;
+  topics.push(animal);
+  makeBtns();
   //clear input
-  document.querySelector("#button-input").value = "";
+  document.querySelector("#add-animal").value = "";
 });
 
 //Function to make buttons
 let makeBtns = () => {
-  document.querySelector("#buttons").innerHTML = "";
+  document.querySelector("#gif-view").innerHTML = "";
   topics.forEach((item, i) => {
+    // let stillUrl =
+    // let animatedUrl =
     let btnEl = document.createElement("button");
     btnEl.textContent = item;
     btnEl.className = "topicBtn";
-    btnEl.setAttribute();
+    document.querySelector("#buttons").append(btnEl);
+    // btnEl.setAttribute("src", still);
+    // btnEl.setAttribute("data-still", still);
   });
 };
